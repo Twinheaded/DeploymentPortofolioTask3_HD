a33108b136b0eb06b925133f612a1a3827eb4512
Twinheaded <54036180+Twinheaded@users.noreply.github.com>
2025-09-14 11:41:23 +1000

    DeploymentPortofolioTask3_HD

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>My HD PHP App</title>
+</head>
+
+<body>
+    <h1>Hello from my PHP App!</h1>
+    <p>The current date and time is <?php echo date('Y-m-d H:i:s'); ?></p>
+</body>
+
+</html>
